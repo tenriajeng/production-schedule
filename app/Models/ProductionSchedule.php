1118512d0c82d6f79c63665ff612c00624e5946a
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2024-11-03 12:08:51 +0800

    first commit

---
+++ app/Models/ProductionSchedule.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductionSchedule extends Model
+{
+    use HasFactory;
+}
